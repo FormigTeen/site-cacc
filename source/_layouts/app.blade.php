ecce828457a655485b936c9c91c1d9c7b8fdcd0c
Matheus Silva Freitas <msformigteen@live.com>
2020-03-07 21:52:45 -0300

    Corrige Slider

--- source/_layouts/app.blade.php
+++ source/_layouts/app.blade.php
@@ -253,7 +253,7 @@
                 @foreach($eventos as $event)
                     <div class="swiper-slide">
                         <div class="event row align-items-center align-items-stretch">
-                            <div class="col-lg-6 pr-lg-0">
+                            <div class="col-md-6 pr-lg-0">
                                 <div class="image cover"><img src="{{ $event->banner_url }}" alt="{{ $event->title }}">
                                     <div class="overlay d-flex align-items-end">
                                         <div class="date">
